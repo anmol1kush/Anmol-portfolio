08294b2ff94e136e5722651951dddb8f0f8403ca
Anmol kushwaha <anmolkush1506@gmail.com>
2025-11-02 01:35:07 +0530

    first commit

---
+++ src/utils/data/personal-data.js
@@ -0,0 +1,15 @@
+export const personalData = {
+  name: "Anmol Kushwaha",
+  profile: '/profile.jpg',
+  designation: "Software Developer",
+  description: "I'm Anmol Kushwaha, a passionate programmer with a love for learning and problem-solving. A quick learner and self-driven, I thrive on exploring new technologies and expanding my skills. My core expertise lies in JavaScript, and I enjoy building solutions with this versatile language. I am deeply interested in web application development and love contributing to making the web more accessible and dynamic. I'm open to job opportunities that align with my skills and interests.",
+  email: 'anmolkush1506@gmail.com',
+  phone: '+91 7080502411',
+  address: 'Gorakhpur, Uttar Pradesh, India',
+  github: 'https://github.com/anmol1kush',
+  linkedIn: 'https://www.linkedin.com/in/anmol-kushwaha-41aa47289/',
+  instagram: 'https://www.instagram.com/_anmol.kush/',
+  leetcode: "",
+  devUsername: "anmolkush",
+  resume: "https://drive.google.com/file/d/1smPZp5c74PKHGnsgG0bomTGVjy6kUisd/view?usp=drive_link"
+}
